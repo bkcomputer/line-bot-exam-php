5bd711b0a257004fb9d6b2980e3a7da370266b9a
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-04 22:07:32 +0700

    Update trello.php

--- trello.php
+++ trello.php
@@ -1,3 +1,13 @@
-<?
-echo "hello";
-?>
+var data = null;
+
+var xhr = new XMLHttpRequest();
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === this.DONE) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("POST", "https://api.trello.com/1/cards?name=test&desc=test&idList=5bdc072ae0605e78a05a92c5&keepFromSource=all&key=de2dc6ea5b95210c5f7ab253415725f7&token=45abc962c53562b0610b6bcf1236ec645dfe9b1f01994469bd591393a1a23443");
+
+xhr.send(data);
