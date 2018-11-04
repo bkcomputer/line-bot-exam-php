5b2bc9b8a48b316d94a79a740ba846a1e82008b8
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-04 18:26:08 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
 
 $access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'Ue5012e668f8c66f02959ac9c23cc3d4b';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
