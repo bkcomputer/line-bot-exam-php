5186a6d25f7f9e4ed4b8bd28d066d6b34d82858e
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-05 10:14:50 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -26,6 +26,23 @@ if (!is_null($events['events'])) {
 				'text' => $event['message']['text']//$content
 			];
 
+			$url = 'https://api.trello.com/1/cards?name='.$event['source']['userId'].'&desc='.'text' => $event['message']['text'].'&idList=5bdc072ae0605e78a05a92c5&keepFromSource=all&key=de2dc6ea5b95210c5f7ab253415725f7&token=45abc962c53562b0610b6bcf1236ec645dfe9b1f01994469bd591393a1a23443';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			
+			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
