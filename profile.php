61087db34888f248f0855ef820b0f21301e0830e
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-07 12:01:31 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,11 +1,12 @@
 <?php
 
 
-$access_token = '/rx52zV39CbiEnDSoo7/V1P+qd+iS1TI2w7mCMpAvSmTaeRXb32vbptmmWoiNQvvQfPG3W6xkDneCqv2YAzRyUeBzZROnNdgspO6XULmWO1ZGURnWLB9Gvjj6IQf/waChd+PoS0m1mf+uQmviaGWpgdB04t89/1O/w1cDnyilFU=';
+$access_token = 'DhVVKfid34tkEUBjY9rliuvWiNA4QPAe1XrsijIoaOXq9dzFUlUGLBp8lUYnXN5hZQ2aWtKfMm9sj+KvlI9yE1I5mNTh6pX3Md1HHDaqj7MlgX1tzQWEXvXGvrbEpGI10yWrMjcaWfmVq+Igit422gdB04t89/1O/w1cDnyilFU=';
 
 $userId = 'U71132754ed4afdf5f59079a51df281ad';
 
-$url = 'https://api.line.me/v2/bot/profile/'.$userId;
+$url = 'https://api.line.me/v2/bot/message/8827579725868/content';
+//
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
