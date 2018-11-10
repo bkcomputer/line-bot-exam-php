ed836515a2d9ba89ba07393abf24356e9d07f208
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-10 21:32:31 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -2,12 +2,17 @@
 
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
+//API Key de2dc6ea5b95210c5f7ab253415725f7
+//Token Authen  45abc962c53562b0610b6bcf1236ec645dfe9b1f01994469bd591393a1a23443
 
 $access_token = '/rx52zV39CbiEnDSoo7/V1P+qd+iS1TI2w7mCMpAvSmTaeRXb32vbptmmWoiNQvvQfPG3W6xkDneCqv2YAzRyUeBzZROnNdgspO6XULmWO1ZGURnWLB9Gvjj6IQf/waChd+PoS0m1mf+uQmviaGWpgdB04t89/1O/w1cDnyilFU=';
 //5bdfd2be5398f03663e5f9c8 Bank
 //5be6b88701327a4ed47f251c Multi
 
+//5be6ea2338cc804e19b8f71b Card
 
+
+//curl -v -F token={45abc962c53562b0610b6bcf1236ec645dfe9b1f01994469bd591393a1a23443} -F file=@test.pdf https://api.trello.com/1/cards/5be6ea2338cc804e19b8f71b/attachments\?key\={de2dc6ea5b95210c5f7ab253415725f7}\&token\={45abc962c53562b0610b6bcf1236ec645dfe9b1f01994469bd591393a1a23443}
 
 // Get POST body content
 $content = file_get_contents('php://input');
