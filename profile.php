dab15a2108d4d51aa06cdb6725f7826d15f7a953
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-10 23:51:00 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,6 +3,28 @@
 
 $access_token = '/rx52zV39CbiEnDSoo7/V1P+qd+iS1TI2w7mCMpAvSmTaeRXb32vbptmmWoiNQvvQfPG3W6xkDneCqv2YAzRyUeBzZROnNdgspO6XULmWO1ZGURnWLB9Gvjj6IQf/waChd+PoS0m1mf+uQmviaGWpgdB04t89/1O/w1cDnyilFU=';
 
+
+
+$url = 'https://api.line.me/v2/bot/message/8843347535283/content';
+//
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
+
+
+<?php
+
+/*
+$access_token = '/rx52zV39CbiEnDSoo7/V1P+qd+iS1TI2w7mCMpAvSmTaeRXb32vbptmmWoiNQvvQfPG3W6xkDneCqv2YAzRyUeBzZROnNdgspO6XULmWO1ZGURnWLB9Gvjj6IQf/waChd+PoS0m1mf+uQmviaGWpgdB04t89/1O/w1cDnyilFU=';
+
 $userId = 'U98ff4f3d94c11a90902cb1a41d0c0388';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
@@ -18,3 +40,5 @@ curl_close($ch);
 
 echo $result;
 
+*/
+??
