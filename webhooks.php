c9a9f9c7f9e7aa3b68038f02f23af3da3482e39e
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-10 16:51:52 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -25,30 +25,31 @@ if (!is_null($events['events'])) {
 			
 			$fullText = $event['message']['text'];
 			
-			$fullText = str_replace( '@Monster_P', '', $fullText);
+			$fullText = str_replace( '@➰°PookPick°➿', '', $fullText);
 			$fullText = str_replace( '@BANK DEV', '', $fullText);
 			$fullText = str_replace( '@Pure', '', $fullText);
-			$fullText = str_replace( '@Ton', '', $fullText);
-			$fullText = str_replace( '@iSymphonyz', '', $fullText);
+			$fullText = str_replace( '@North', '', $fullText);
+			$fullText = str_replace( '@けんじ', '', $fullText);
 			$fullText = str_replace( '@เสี่ยหมี', '', $fullText);
-			$fullText = str_replace( '@KinG`Genesis', '', $fullText);
+			
 			
 
 			$head = urlencode($fullText)."-".date('m/d/Y');
 			
 			
-			//5be2658aa5b1b615863f6d45 PM @Monster_P @BANK DEV
-			//5be26448b284fe18ca0f60e4 Design @Pure
-			//5be26421e7ef14154e8e1719 IOS @Ton
-			//5be2642571a7725bc73d523b Android @iSymphonyz
-			//5be264422464ad09fa4116bf CMS @เสี่ยหมี @KinG`Genesis
+			//5bdc072ae0605e78a05a92c5 PM @➰°PookPick°➿ @BANK DEV
+			//5bdc0ae66e82f44f8dc9506b Design @Pure
+			//5bdc0730f8bb4e8b70992f25 IOS @North
+			//5bdc0927cf837652b9c2e2b1 Android @けんじ
+			//5bdc073f5622911911691960 CMS @เสี่ยหมี 
+			//5bdc07424a0dda5b0a113620 Deploy @Poo_za
 		
 			$board = "";
-			if (strpos($event['message']['text'], '@Monster_P') !== false) {
+			if (strpos($event['message']['text'], '@➰°PookPick°➿') !== false) {
 			    $board = "5be2658aa5b1b615863f6d45";
 				
 				$url = 'https://api.trello.com/1/cards?name=';
-			$url .= urlencode('@Monster_P').$head;
+			$url .= urlencode('@➰°PookPick°➿').$head;
 			
 			//$url .= $event['source']['userId'];
 			$url .='&desc=';
@@ -233,11 +234,11 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 				
-			} if (strpos($event['message']['text'], '@Ton') !== false) {
+			} if (strpos($event['message']['text'], '@North') !== false) {
 			    $board = "5be26421e7ef14154e8e1719";
 				
 				$url = 'https://api.trello.com/1/cards?name=';
-			$url .= urlencode('@Ton').$head;
+			$url .= urlencode('@North').$head;
 			
 			//$url .= $event['source']['userId'];
 			$url .='&desc=';
@@ -296,11 +297,11 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 				
-			} if (strpos($event['message']['text'], '@iSymphonyz') !== false) {
+			} if (strpos($event['message']['text'], '@けんじ') !== false) {
 			    $board = "5be2642571a7725bc73d523b";
 				
 				$url = 'https://api.trello.com/1/cards?name=';
-			$url .= urlencode('@iSymphonyz').$head;
+			$url .= urlencode('@けんじ').$head;
 			
 			//$url .= $event['source']['userId'];
 			$url .='&desc=';
@@ -422,71 +423,7 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 				
-			} if (strpos($event['message']['text'], '@KinG`Genesis') !== false) {
-			    $board = "5be264422464ad09fa4116bf";
-				
-				$url = 'https://api.trello.com/1/cards?name=';
-			$url .= urlencode('@KinG`Genesis').$head;
-			
-			//$url .= $event['source']['userId'];
-			$url .='&desc=';
-			
-			
-			
-			$url .= urlencode($fullText);
-			
-			
-			$url .='&idList=';
-			$url .=$board;
-			$url .='&keepFromSource=all&key=de2dc6ea5b95210c5f7ab253415725f7&token=45abc962c53562b0610b6bcf1236ec645dfe9b1f01994469bd591393a1a23443';
-			
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$fullText],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-			
-			
-			$respones = json_decode($result, true);
-			
-			$messages = [
-				'type' => 'text',
-				'text' => $respones['shortUrl']//$content
-			];
-			
-			
-			
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-				
-			}
-// 			}else{
+			} else{
 // 			    $board = "5be2658aa5b1b615863f6d45";
 			
 // 			}
