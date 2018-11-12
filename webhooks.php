522ea93aebe0578135b8a65cc75d039bf58903e8
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-12 20:36:19 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -5,10 +5,10 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$time = strtotime('p/16/2003');
+//$time = strtotime('p/16/2003');
 
 try {
-  $time = strtotime('10/16/2003');
+  $time = strtotime('p/16/2003');
 } catch(Exception $e) {
   echo "Invalid date... {$e->getMessage()}";
 }
