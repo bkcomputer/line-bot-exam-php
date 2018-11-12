9c40336cac205e084bcb444eaad234632d199bb8
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-12 20:51:37 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -6,7 +6,7 @@ require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 
-echo substr("aaaaaa due 01/02/2018", -10);  // bcd
+echo substr("aaaaaa due 2018/10/2", -10);  // bcd
 exit;
 
 $access_token = 'DhVVKfid34tkEUBjY9rliuvWiNA4QPAe1XrsijIoaOXq9dzFUlUGLBp8lUYnXN5hZQ2aWtKfMm9sj+KvlI9yE1I5mNTh6pX3Md1HHDaqj7MlgX1tzQWEXvXGvrbEpGI10yWrMjcaWfmVq+Igit422gdB04t89/1O/w1cDnyilFU=';
