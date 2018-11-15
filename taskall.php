2bce224da6030626ebf7d1dfecf4d4cb2613cf7c
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-15 12:49:58 +0700

    Update taskall.php

--- taskall.php
+++ taskall.php
@@ -1,34 +1,27 @@
 <?php
 
 $url = "https://api.trello.com/1/lists/5be2658aa5b1b615863f6d45/cards?key=de2dc6ea5b95210c5f7ab253415725f7&token=45abc962c53562b0610b6bcf1236ec645dfe9b1f01994469bd591393a1a23443";
-			# init curl
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $url);
-			//curl_setopt($ch, CURLOPT_POSTFIELDS, $encoded_fields);
-			curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
-			curl_setopt($ch, CURLOPT_HEADER, 1);
-			curl_setopt($ch, CURLINFO_HEADER_OUT, TRUE); // make sure we see the sended header afterwards
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			//curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-			curl_setopt($ch, CURLOPT_TIMEOUT, 0);
-			//curl_setopt($ch, CURLOPT_POST, 1);
-			# dont care about ssl
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			# download and close
-			$output = curl_exec($ch);
-			$request =  curl_getinfo($ch, CURLINFO_HEADER_OUT);
-			$error = curl_error($ch);
 			
-			
-			
-			curl_close($ch);
-			
-			
-			//echo 'This is output = '.$output .'<br />';
-			//echo 'This is request = '.$request .'<br />';
-			//echo 'This is error = '.$error .'<br />';
+$access_token = "45abc962c53562b0610b6bcf1236ec645dfe9b1f01994469bd591393a1a23443";
 
-			echo "<pre>";
-			print_r(json_decode($output));
+
+
+			// Make a POST Request to Messaging API to reply to sender
+			//$url = 'https://api.trello.com/1/cards?name=phmee&desc=takeashower&idList=5bdc072ae0605e78a05a92c5&keepFromSource=all&key=de2dc6ea5b95210c5f7ab253415725f7&token=45abc962c53562b0610b6bcf1236ec645dfe9b1f01994469bd591393a1a23443';
+			//$data = [
+			//	'replyToken' => $replyToken,
+			//	'messages' => [$messages],
+			//];
+			//$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			//curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
 
 ?>
