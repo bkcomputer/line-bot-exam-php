71d756a4f667a665f38535deb8fa3cf861ca6fb9
bkcomputer <pongsakorn.dev@gmail.com>
2018-11-16 16:37:13 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -151,36 +151,7 @@ if (!is_null($events['events'])) {
 
 
 				}
-				if (strpos($event['message']['text'], 'Asynchronous') !== false) {
-					$board = "5bee8458b952f719343aae26";
-
-					$url = 'https://api.trello.com/1/cards?name=';
-					$url .= urlencode('Asynchronous').$head;
-
-					$url .='&desc=' . $event['source']['userId'];
-					$url .= urlencode($fullText);
-
-
-					$url .='&idList=';
-					$url .=$board;
-					$due = $duedate;
-					$url .='&pos=top&due=' . $due;
-					$url .='&keepFromSource=all&key=de2dc6ea5b95210c5f7ab253415725f7&token=45abc962c53562b0610b6bcf1236ec645dfe9b1f01994469bd591393a1a23443';
-
-
-					$post = json_encode($data);
-					$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-					$ch = curl_init($url);
-					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-					$result = curl_exec($ch);
-					curl_close($ch);
-
-
-				}
+				
 				
 				
 				
@@ -439,6 +410,36 @@ if (!is_null($events['events'])) {
 
 
 				} 
+				if (strpos($event['message']['text'], 'Asynchronous') !== false) {
+					$board = "5bee8458b952f719343aae26";
+
+					$url = 'https://api.trello.com/1/cards?name=';
+					$url .= urlencode('Asynchronous').$head;
+
+					$url .='&desc=' . $event['source']['userId'];
+					$url .= urlencode($fullText);
+
+
+					$url .='&idList=';
+					$url .=$board;
+					$due = $duedate;
+					$url .='&pos=top&due=' . $due;
+					$url .='&keepFromSource=all&key=de2dc6ea5b95210c5f7ab253415725f7&token=45abc962c53562b0610b6bcf1236ec645dfe9b1f01994469bd591393a1a23443';
+
+
+					$post = json_encode($data);
+					$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+					$ch = curl_init($url);
+					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+					$result = curl_exec($ch);
+					curl_close($ch);
+
+
+				}
            
 			}	
 			
