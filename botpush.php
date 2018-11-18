d273b3a759cfea07ccb7082775951014ad67694f
(BankK)pongsakorn ploysaruk <pongsakorn.p@360innovative.com>
2018-11-18 11:47:21 +0700

    add goupr

--- botpush.php
+++ botpush.php
@@ -8,7 +8,7 @@ $access_token = '/rx52zV39CbiEnDSoo7/V1P+qd+iS1TI2w7mCMpAvSmTaeRXb32vbptmmWoiNQv
 
 $channelSecret = '6ef616402eb039e9e57916e124de0e4d';
 
-$pushID = 'Ue5012e668f8c66f02959ac9c23cc3d4b';//'C330aac644f741be04358c14de9ca0d2a';//'U400a81891a5ff558bd4d6c15adf9b16a';//
+$pushID = 'C330aac644f741be04358c14de9ca0d2a';//'Ue5012e668f8c66f02959ac9c23cc3d4b';//'C330aac644f741be04358c14de9ca0d2a';//'U400a81891a5ff558bd4d6c15adf9b16a';//
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
@@ -20,13 +20,13 @@ echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
+// $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+// $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage('<to>', $textMessageBuilder);
+// $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+// $response = $bot->pushMessage('<to>', $textMessageBuilder);
 
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+// echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 
 // $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
